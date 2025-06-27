4da0757cadecd8b2401c3bb4036e062bf306f977
bruno-fernandes18 <148147246+bruno-fernandes18@users.noreply.github.com>
2025-06-27 15:38:02 -0300

    Expand databases with additional names, cities, stadiums and clubs

--- core/data/times_db.py
+++ core/data/times_db.py
@@ -1,8 +1,53 @@
 """Lista de times do Brasileirão."""
 
-TIMES_BRASILEIRO = [
+# Lista de clubes participantes de competições nacionais. Para permitir
+# campeonatos mais amplos, inclui times da Série A e da Série B.
+
+TIMES_SERIE_A = [
     "Flamengo",
     "Palmeiras",
     "Santos",
     "Corinthians",
+    "Fluminense",
+    "Athletico Paranaense",
+    "Atletico Mineiro",
+    "Gremio",
+    "Internacional",
+    "Fortaleza",
+    "Bahia",
+    "Botafogo",
+    "Bragantino",
+    "Cuiaba",
+    "Goias",
+    "Sao Paulo",
+    "Vasco",
+    "Cruzeiro",
+    "Coritiba",
+    "America Mineiro",
 ]
+
+TIMES_SERIE_B = [
+    "Sport",
+    "Ceara",
+    "Chapecoense",
+    "Ponte Preta",
+    "Avai",
+    "Guarani",
+    "Vitoria",
+    "CRB",
+    "Goias",
+    "Juventude",
+    "Londrina",
+    "Ituano",
+    "Sampaio Correa",
+    "Operario",
+    "ABC",
+    "Vila Nova",
+    "Tombense",
+    "Mirassol",
+    "Botafogo-SP",
+    "Paysandu",
+]
+
+# Conjunto completo utilizado pela aplicação em competições.
+TIMES_BRASILEIRO = TIMES_SERIE_A + TIMES_SERIE_B
