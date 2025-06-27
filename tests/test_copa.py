fc97b736d626e0610f49d9c92a478f95a7049b77
bruno-fernandes18 <148147246+bruno-fernandes18@users.noreply.github.com>
2025-06-27 15:09:52 -0300

    Add default start date to Calendario

--- tests/test_copa.py
+++ tests/test_copa.py
@@ -1,4 +1,3 @@
-from datetime import date
 from core.entities.copa import Copa
 from core.entities.time import Time
 
@@ -7,9 +6,10 @@ def test_copa_gera_chave_datas():
     copa = Copa('Copa', 2023)
     times = [Time(str(i), str(i), 1900, 'X', 'Y') for i in range(4)]
     copa.times = times
-    copa.calendario.data_inicio = date(2023, 1, 1)
+    data_inicio = copa.calendario.data_inicio
     copa.gerar_calendario()
     assert len(copa.partidas) == 3
     datas = [p.data for p in copa.calendario.partidas]
+    assert datas[0] == data_inicio
     for d1, d2 in zip(datas, datas[1:]):
         assert (d2 - d1).days >= 3
